eb6ace402e4130d1c67351bb402ea5b6f054987b
Ashwitha <ashwithabg@gmail.com>
2021-12-28 09:46:48 +0530

    max depth BFS

--- Tree/problems/maxDepth/main.go
+++ Tree/problems/maxDepth/main.go
@@ -8,6 +8,32 @@ import (
 func main() {
 	root := utils.GetNumbericRootNode()
 	fmt.Println(maxDepth(root))
+	fmt.Println(maxDepthBFS(root))
+}
+
+func maxDepthBFS(root *utils.Node) int {
+	depth := 0
+	queue := utils.NewQueue()
+	queue.Enqueue(root)
+
+	for !queue.IsEmpty() {
+		depth++
+
+		size := queue.Size()
+		for ;size > 0; size-- {
+			node := queue.Dequeue()
+			if node.GetLeftNode() != nil {
+				queue.Enqueue(node.GetLeftNode())
+			}
+
+			if node.GetRightNode() != nil {
+				queue.Enqueue(node.GetRightNode())
+			}
+		}
+
+	}
+
+	return depth
 }
 
 func maxDepth(root *utils.Node) int {
