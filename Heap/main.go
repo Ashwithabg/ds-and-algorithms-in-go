18670ba2d38e522634a11dccb0018d9b41e0accb
Ashwitha <ashwithabg@gmail.com>
2021-12-31 10:52:19 +0530

    Heapify

--- Heap/main.go
+++ Heap/main.go
@@ -1,6 +1,9 @@
 package main
 
+import "fmt"
+
 const MaxSize = 40
+const rootIndex = 0
 
 type Heap struct {
 	data []interface{}
@@ -98,5 +101,87 @@ func (h *Heap) siftUp(index int) {
 	if parentIndex != -1 && h.getElement(index).(int) < h.getElement(parentIndex).(int) {
 		h.swap(index, parentIndex)
 		h.siftUp(parentIndex)
+	}
+}
+
+func (h *Heap) insert(value int) error {
+	if h.isFull() {
+		return fmt.Errorf("heap is full")
+	}
+	count := len(h.data)
+	h.data = append(h.data, value)
+	h.siftUp(count)
+
+	return nil
+}
+
+func (h *Heap) remove() (int, error) {
+	if h.isEmpty() {
+		return rootIndex, fmt.Errorf("heap is empty")
+	}
+	highPriorityElement := h.data[rootIndex]
+	lastIndex := len(h.data) - 1
+
+	h.swap(rootIndex, lastIndex)
+	h.data = h.data[:lastIndex]
+	h.siftDown(rootIndex)
+
+	return highPriorityElement.(int), nil
+}
+
+//maxHeap
+func (h *Heap) findIndex(index, endIndex int, fn func(int) int) int {
+	if index > endIndex || index == -1 {
+		return -1
+	}
+
+	childIndex := fn(index)
+	if childIndex > endIndex || childIndex < 0 {
+		return -1
+	}
+
+	return childIndex
+}
+
+func (h *Heap) findLeftChildIndex(index, endIndex int) int {
+	indexCalculator := func(index int) int {
+		return 2*index + 1
+	}
+	return h.findIndex(index, endIndex, indexCalculator)
+}
+
+func (h *Heap) findRightChildIndex(index, endIndex int) int {
+	indexCalculator := func(index int) int {
+		return 2*index + 2
+	}
+	return h.findIndex(index, endIndex, indexCalculator)
+}
+
+func (h *Heap) findParentIndex(index, endIndex int) int {
+	indexCalculator := func(index int) int {
+		return (index - 1) / 2
+	}
+	return h.findIndex(index, endIndex, indexCalculator)
+}
+
+func (h *Heap) percolateDown(index, endIndex int) {
+	leftChildIndex := h.findLeftChildIndex(index, endIndex)
+	rightChildIndex := h.findRightChildIndex(index, endIndex)
+
+	if leftChildIndex != -1 && h.getElement(leftChildIndex).(int) > h.getElement(index).(int) {
+		h.swap(leftChildIndex, index)
+		h.percolateDown(leftChildIndex, endIndex)
+	}
+	if rightChildIndex != -1 && h.getElement(rightChildIndex).(int) > h.getElement(index).(int) {
+		h.swap(rightChildIndex, index)
+		h.percolateDown(rightChildIndex, endIndex)
+	}
+}
+
+func (h *Heap) Heapify(endIndex int) {
+	parentIndex := h.findParentIndex(endIndex, endIndex)
+	for parentIndex >= 0 {
+		h.percolateDown(parentIndex, endIndex)
+		parentIndex--
 	}
 }
